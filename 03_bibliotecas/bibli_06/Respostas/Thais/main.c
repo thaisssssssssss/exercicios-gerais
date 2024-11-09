451c4f7c2bc83282fa1d31c6b3b32574c83b1326
thaisssssssssss <thaisgomes1585@gmail.com>
2024-11-09 16:00:22 -0300

    ajustes

--- 03_bibliotecas/bibli_06/Respostas/Thais/main.c
+++ 03_bibliotecas/bibli_06/Respostas/Thais/main.c
@@ -13,20 +13,20 @@ int main(){
             printf("Temperature: %.2fK", convert_celsius_to_kelvin(temperature));
         }
         else if(conversor == 'f'){
-            printf("Temperature: %.2fF", convert_celsius_to_fahrenheit(temperature));
+            printf("Temperature: %.2fFº", convert_celsius_to_fahrenheit(temperature));
         }
     }
     else if(currentTemperature == 'k'){
         if(conversor == 'c'){
-            printf("Temperature: %.2fC", convert_kelvin_to_celsius(temperature));
+            printf("Temperature: %.2fCº", convert_kelvin_to_celsius(temperature));
         }
         else if(conversor == 'f'){
-            printf("Temperature: %.2fF", convert_kelvin_to_fahrenheit(temperature));
+            printf("Temperature: %.2fFº", convert_kelvin_to_fahrenheit(temperature));
         }
     }
     else if(currentTemperature == 'f'){
         if(conversor == 'c'){
-            printf("Temperature: %.2fC", convert_fahrenheit_to_celsius(temperature));
+            printf("Temperature: %.2fCº", convert_fahrenheit_to_celsius(temperature));
         }
         else if(conversor == 'k'){
             printf("Temperature: %.2fK", convert_fahrenheit_to_kelvin(temperature));
