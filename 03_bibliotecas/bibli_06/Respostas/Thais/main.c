ebaaf1f1c71b0a47f0096d3174f0604f39f0b617
thaisssssssssss <thaisgomes1585@gmail.com>
2024-11-09 15:44:25 -0300

    questao 06_bibliotecas

---
+++ 03_bibliotecas/bibli_06/Respostas/Thais/main.c
@@ -0,0 +1,35 @@
+#include "temperature_conversor.h"
+#include <stdio.h>
+
+int main(){
+    float temperature;
+    char currentTemperature, conversor;
+
+    scanf("%f ", &temperature);
+    scanf("%c %c", &currentTemperature, &conversor);
+
+    if(currentTemperature == 'c'){
+        if(conversor == 'k'){
+            printf("Temperature: %.2fK", convert_celsius_to_kelvin(temperature));
+        }
+        else if(conversor == 'f'){
+            printf("Temperature: %.2fF", convert_celsius_to_fahrenheit(temperature));
+        }
+    }
+    else if(currentTemperature == 'k'){
+        if(conversor == 'c'){
+            printf("Temperature: %.2fC", convert_kelvin_to_celsius(temperature));
+        }
+        else if(conversor == 'f'){
+            printf("Temperature: %.2fF", convert_kelvin_to_fahrenheit(temperature));
+        }
+    }
+    else if(currentTemperature == 'f'){
+        if(conversor == 'c'){
+            printf("Temperature: %.2fC", convert_fahrenheit_to_celsius(temperature));
+        }
+        else if(conversor == 'k'){
+            printf("Temperature: %.2fK", convert_fahrenheit_to_kelvin(temperature));
+        }
+    }
+}
