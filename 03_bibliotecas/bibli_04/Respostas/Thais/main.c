8e1efc962462ca040bd08cf592576391c1a2c97f
thaisssssssssss <thaisgomes1585@gmail.com>
2024-11-09 14:08:33 -0300

    adicionando conteudo

---
+++ 03_bibliotecas/bibli_04/Respostas/Thais/main.c
@@ -0,0 +1,37 @@
+#include "data.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int dia, mes, ano;
+    int dataValida;
+    int bissexto;
+    int diasMes;
+
+    int maiorData;
+    int diferencaDias;
+
+    scanf("%d/%d/%d", &dia, &mes, &ano);
+
+    dataValida = verificaDataValida(dia, mes, ano);
+    if(!(dataValida)){
+        printf("A data informada eh invalida\n");
+        exit(0);
+    }
+
+    printf("Data informada: ");
+    imprimeDataExtenso(dia, mes, ano);
+
+    bissexto = verificaBissexto(ano);
+    if(bissexto){
+        printf("O ano informado eh bissexto\n");
+    }
+    else{
+        printf("O ano informado nao eh bissexto\n");
+    }
+
+    diasMes = numeroDiasMes(mes, ano);
+    printf("O mes informado possui %d dias\n", diasMes);
+
+    imprimeProximaData(dia, mes, ano);
+}
