c5c4fd0eea31192ff123ce7401ebd699f29594a2
Thais Gomes Brandão <thaisgomes1585@gmail.com>
2024-11-25 03:12:34 +0000

    tad 09

--- 04_TAD_simples/TAD_08/empresa.c
+++ 04_TAD_simples/TAD_08/empresa.c
@@ -2,25 +2,16 @@
 #include "funcionario.h"
 #include <stdio.h>
 
-/**
- * @brief Cria uma empresa a partir de um id.
- * 
- * @param id Id do empresa a ser criada.
- * @return TEmpresa empresa criada a partir do Id fornecido.
- */
 tEmpresa criaEmpresa(int id){
     tEmpresa empresa;
     int i;
+    for(i = 0; i < 20; i++){
+        empresa.funcionarios[i].id = 0;
+    }
     empresa.id = id;
-    empresa.qtdFuncionarios = 0;
     return empresa;
 }
 
-/**
- * @brief Le os dados de uma empresa da entrada padrao.
- * 
- * @return TEmpresa empresa criada a partir dos dados lidos.
- */
 tEmpresa leEmpresa(){
     tEmpresa empresa;
     tFuncionario funcionario;
@@ -28,53 +19,45 @@ tEmpresa leEmpresa(){
 
     scanf("%d %d", &id, &qtdFuncionarios);
     empresa = criaEmpresa(id);
-    
-    for(i = 0; i < qtdFuncionarios; i++){
+    empresa.qtdFuncionarios = qtdFuncionarios;
+    for(i = 0; i < empresa.qtdFuncionarios; i++){
         funcionario = leFuncionario();
         empresa = contrataFuncionarioEmpresa(empresa, funcionario);
-        empresa.qtdFuncionarios++;
     }
-
-    return empresa;
 }
 
-/**
- * @brief Adiciona um funcionario a uma empresa caso o id do funcionario ainda nao esteja registrado na empresa.
- * 
- * @param empresa Empresa em que o funcionario deve ser adicionado.
- * @param funcionario Funcionario que deve ser adicionado a empresa.
- * @return TEmpresa empresa atualizada com a nova lista de funcionarios.
- */
 tEmpresa contrataFuncionarioEmpresa(tEmpresa empresa, tFuncionario funcionario){
     int i;
     int contratado = 0;
     for(i = 0; i < empresa.qtdFuncionarios; i++){
         if(funcionario.id == empresa.funcionarios[i].id){
             contratado = 1;
-            empresa.qtdFuncionarios--;
             printf("A empresa %d ja possui um funcionario com o id %d\n", empresa.id, funcionario.id);    
             break;
         }
     }
 
     if(contratado == 0){
-        empresa.funcionarios[empresa.qtdFuncionarios] = funcionario;
+        for(i = 0; i < empresa.qtdFuncionarios; i++){
+            if(empresa.funcionarios[i].id == 0){
+                empresa.funcionarios[i].id = funcionario.id;
+                empresa.funcionarios[i].salario = funcionario.salario;
+                break;
+            }
+        }
     }
 
     return empresa;
 }
 
-/**
- * @brief Imprime todos os dados de um empresa.
- * 
- * @param empresa Empresa a ser impressa no terminal.
- */
 void imprimeEmpresa(tEmpresa empresa){
     int i;
-    if(empresa.id != 0)
     printf("Empresa %d:\n", empresa.id);
 
     for(i = 0; i < empresa.qtdFuncionarios; i++){
-        imprimeFuncionario(empresa.funcionarios[i]);
+        if(empresa.funcionarios[i].id != 0){
+            imprimeFuncionario(empresa.funcionarios[i]);
+        }
     }
 }
+
--- 04_TAD_simples/TAD_09/Resultados/Thais/empresa/empresa.c
+++ 04_TAD_simples/TAD_09/Resultados/Thais/empresa/empresa.c
@@ -2,16 +2,25 @@
 #include "funcionario.h"
 #include <stdio.h>
 
+/**
+ * @brief Cria uma empresa a partir de um id.
+ * 
+ * @param id Id do empresa a ser criada.
+ * @return TEmpresa empresa criada a partir do Id fornecido.
+ */
 tEmpresa criaEmpresa(int id){
     tEmpresa empresa;
     int i;
-    for(i = 0; i < 20; i++){
-        empresa.funcionarios[i].id = 0;
-    }
     empresa.id = id;
+    empresa.qtdFuncionarios = 0;
     return empresa;
 }
 
+/**
+ * @brief Le os dados de uma empresa da entrada padrao.
+ * 
+ * @return TEmpresa empresa criada a partir dos dados lidos.
+ */
 tEmpresa leEmpresa(){
     tEmpresa empresa;
     tFuncionario funcionario;
@@ -19,45 +28,53 @@ tEmpresa leEmpresa(){
 
     scanf("%d %d", &id, &qtdFuncionarios);
     empresa = criaEmpresa(id);
-    empresa.qtdFuncionarios = qtdFuncionarios;
-    for(i = 0; i < empresa.qtdFuncionarios; i++){
+    
+    for(i = 0; i < qtdFuncionarios; i++){
         funcionario = leFuncionario();
         empresa = contrataFuncionarioEmpresa(empresa, funcionario);
+        empresa.qtdFuncionarios++;
     }
+
+    return empresa;
 }
 
+/**
+ * @brief Adiciona um funcionario a uma empresa caso o id do funcionario ainda nao esteja registrado na empresa.
+ * 
+ * @param empresa Empresa em que o funcionario deve ser adicionado.
+ * @param funcionario Funcionario que deve ser adicionado a empresa.
+ * @return TEmpresa empresa atualizada com a nova lista de funcionarios.
+ */
 tEmpresa contrataFuncionarioEmpresa(tEmpresa empresa, tFuncionario funcionario){
     int i;
     int contratado = 0;
     for(i = 0; i < empresa.qtdFuncionarios; i++){
         if(funcionario.id == empresa.funcionarios[i].id){
             contratado = 1;
+            empresa.qtdFuncionarios--;
             printf("A empresa %d ja possui um funcionario com o id %d\n", empresa.id, funcionario.id);    
             break;
         }
     }
 
     if(contratado == 0){
-        for(i = 0; i < empresa.qtdFuncionarios; i++){
-            if(empresa.funcionarios[i].id == 0){
-                empresa.funcionarios[i].id = funcionario.id;
-                empresa.funcionarios[i].salario = funcionario.salario;
-                break;
-            }
-        }
+        empresa.funcionarios[empresa.qtdFuncionarios] = funcionario;
     }
 
     return empresa;
 }
 
+/**
+ * @brief Imprime todos os dados de um empresa.
+ * 
+ * @param empresa Empresa a ser impressa no terminal.
+ */
 void imprimeEmpresa(tEmpresa empresa){
     int i;
+    if(empresa.id != 0)
     printf("Empresa %d:\n", empresa.id);
 
     for(i = 0; i < empresa.qtdFuncionarios; i++){
-        if(empresa.funcionarios[i].id != 0){
-            imprimeFuncionario(empresa.funcionarios[i]);
-        }
+        imprimeFuncionario(empresa.funcionarios[i]);
     }
 }
-
