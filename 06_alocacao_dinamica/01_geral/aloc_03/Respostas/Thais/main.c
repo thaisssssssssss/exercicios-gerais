a86fc194ec4fb5ddc4e775876603f0fca38c696e
thaisssssssssss <thaisgomes1585@gmail.com>
2025-01-22 21:10:22 -0300

    questoes aloc

---
+++ 06_alocacao_dinamica/01_geral/aloc_03/Respostas/Thais/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utils_char.h"
+
+int main(){
+    int tamanho;
+    scanf("%d", &tamanho);
+    char *vet = CriaVetor(tamanho);
+    ImprimeString(vet, tamanho);
+
+
+    LeVetor(vet, tamanho);
+    
+    ImprimeString(vet, tamanho);
+    LiberaVetor(vet);
+    return 0;
+}
