74c23c610851859eb7b34312006a6ba29d8f7657
thaisssssssssss <thaisgomes1585@gmail.com>
2025-02-01 23:07:16 -0300

    tad_pont_09

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_09/Resultados/Thais/funcionario/funcionario.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funcionario.h"
+
+/**
+ * @brief Cria um funcionario "vazio" 
+ * Seus atributos devem ser inicializados com NULL (ponteiros), ou -1 caso contrário
+ * @return TFuncionario Ponteiro para Funcionario criado a partir do nome e salario fornecidos.
+ */
+tFuncionario* CriaFuncionario(){
+    tFuncionario *f = malloc(sizeof(tFuncionario));
+    f->id = -1;
+    f->salario = -1;
+    return f;
+}
+
+/**
+ * @brief Libera a memória alocada dinamicamente por um funcionário
+ * @param funcionario Ponteiro para Funcionario que terá sua memória liberada
+ */
+void ApagaFuncionario(tFuncionario* funcionario){
+    free(funcionario);
+}
+
+/**
+ * @brief Le os dados de um funcionario da entrada padrao.
+ * 
+ * @param funcionario Funcionario criado a partir dos dados lidos.
+ */
+void LeFuncionario(tFuncionario* funcionario){
+    //funcionario = CriaFuncionario();
+    scanf("%d %f", &funcionario->id, &funcionario->salario);
+}
+
+/**
+ * @brief Obtem o Id de um funcionario.
+ * 
+ * @param funcionario Funcionario que deseja obter o id.
+ * @return int representando o id do funcionario fornecido.
+ */
+int GetIdFuncionario(tFuncionario* funcionario){
+    return funcionario->id;
+}
+
+/**
+ * @brief Imprime todos os dados de um funcionario.
+ * 
+ * @param funcionario Funcionario a ser impresso no terminal.
+ */
+void ImprimeFuncionario(tFuncionario* funcionario){
+    printf("- Funcionario %d: RS %.2f\n", funcionario->id, funcionario->salario);
+}
+
